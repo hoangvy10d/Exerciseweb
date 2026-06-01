707495e9b2b5afa16605c82b787f8e0da6a7fc73
hoangvy10d <nguyenhoangvy0545@gmail.com>
2026-06-01 21:29:29 +0700

    Add files via upload

---
+++ exJava/vid48/main.js
@@ -0,0 +1,23 @@
+function thucHienPhepToan(a, b,pt){
+    kq=""
+switch(pt)
+{
+    case "+":
+kq= (a+b)
+break
+case "-":
+kq=(a-b)
+break
+case "*":
+kq=(a*b)
+break
+case "/":
+kq=(a/b)
+break
+default:
+kq="Phep toan khong hop le"
+break
+}
+return kq
+}
+
